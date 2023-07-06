7326287760509f759216bfe2abe0185a65476fe8
Carl Gardiner <carl.gardiner@os.uk>
2023-07-06 19:21:46 +0100

    initial commit

---
+++ start_potion_quest.exs
@@ -0,0 +1,2 @@
+IO.puts("Welcome to PotionQuest!")
+IO.puts("You are on the journey around the world of Xaalabar to discover all the tonics, tinctures, elixirs and potions your little alchemist hands can discover!")
